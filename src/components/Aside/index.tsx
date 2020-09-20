27aa43e78e003057c0ba7e66e9c11596fb62321e
Lauan Guermandi <lauanguermandi@gmail.com>
2020-09-19 21:35:34 -0300

    feat: add menu on aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react'
 
-import { Container } from './styles'
+import logo from './../../assets/logo.svg'
+
+import {
+  MdDashboard,
+  MdArrowUpward,
+  MdArrowDownward,
+  MdExitToApp
+} from 'react-icons/md'
+
+import { 
+  Container,
+  Header,
+  LogoImg,
+  Title,
+  MenuContainer,
+  MenuItemLink
+ } from './styles'
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logo} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink>
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink>
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink>
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+        
+        <MenuItemLink>
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>  
     </Container>
   )
 }
--- src/components/Toggle/styles.ts
+++ src/components/Toggle/styles.ts
@@ -18,4 +18,3 @@ export const ToggleSelector = styled(Switch).attrs<ReactSwitchProps>(
     }
   )
 )<ReactSwitchProps>``
-
